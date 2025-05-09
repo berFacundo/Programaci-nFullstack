f426693a3374f93bcf74b3bbcddead154dbe61be
Facundo <fberazategui@estudiante.ceibal.edu.uy>
2025-05-09 14:31:01 -0300

    Agrego ejercicio (estructura foreach)

---
+++ Bucles/estructura_foreach.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Ejercicio 1</h1><hr><br>";
+        $nombres = ["Ana", "Luis", "Pedro", "María"];
+
+        foreach ($nombres as $nombre) {
+        echo $nombre . "<br>";
+        }
+    echo "<h1>Ejercicio 2</h1><hr><br>";
+        $productos = [ "Pan" => 40, "Leche" => 60, "Queso" => 120 ];
+
+    foreach ($productos as $nombre => $precio) {
+        echo "Producto: $nombre - Precio: $precio<br>";
+    }
+    ?>
+</body>
+</html>
