66e3427b09489db395bd1e2f7f5b8e9b44ad1498
Facundo <facuberazategui77@gmail.com>
2025-06-03 00:56:56 -0300

    Arreglos avance

--- Practico2B/index.php
+++ Practico2B/index.php
@@ -8,17 +8,27 @@
 <body>
     <?php
     $equipos = [
-            $peñarol = [
-                ["nombre" => "Peñarol",
-                 "puntos" => 6,
-                 "partidosGanados" => 2,
-                 "partidosEmpatados" => 0,
-                 "partidosPerdidos" => 0,
-                 "golesAfavor" => 4,
-                 "golesContra" => 1,
-                 "dif" => 3
-            ];
-    ]
+        [
+            "nombre" => "Peñarol",
+            "puntos" => 6,
+            "partidosGanados" => 2,
+            "partidosEmpatados" => 0,
+            "partidosPerdidos" => 0,
+            "golesAfavor" => 4,
+            "golesContra" => 1,
+            "dif" => 3
+        ],
+        [
+            "nombre" => "Nacional",
+            "puntos" => 4,
+            "partidosGanados" => 1,
+            "partidosEmpatados" => 1,
+            "partidosPerdidos" => 0,
+            "golesAfavor" => 3,
+            "golesContra" => 2,
+            "dif" => 1
+        ]
+        ];
     ?>
 </body>
 </html>
